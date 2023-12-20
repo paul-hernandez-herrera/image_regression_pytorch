ff07eeb71295431e400602a10d3e52dfdcaa6320
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2023-12-20 10:06:14 -0600

    adding horizontal flip for augmentation

--- core_code/Dataset/data_augmentation_classification.py
+++ core_code/Dataset/data_augmentation_classification.py
@@ -1,6 +1,10 @@
 from torchvision.transforms.functional import affine, hflip, vflip
 from torchvision.transforms import Resize
+import torchvision.transforms.v2 as transforms
+from torchvision import datapoints
+import torchvision
 import numpy as np
+from torch import tensor
 
 class augmentation_classification_task():
     def __init__(self, zoom_range = [0.8, 1.2],
@@ -23,11 +27,11 @@ class augmentation_classification_task():
         
         self.transform_resize = Resize((img_resize[0],img_resize[1]), antialias='True')
         
-    def horizontal_flip(self, image):        
+    def horizontal_flip(self, image, bboxes):        
         #random horizontal flip
-        if np.random.uniform(0, 1) > 0.5:
-            image = hflip(image)
-        return image
+        trans = transforms.Compose([transforms.RandomHorizontalFlip(p=0)])
+        image, bboxes, labels = trans(image, bboxes, np.ones((bboxes.shape[0], 1)))
+        return image, bboxes
     
     def vertical_flip(self, image):        
         #random vertical flip    
@@ -36,6 +40,7 @@ class augmentation_classification_task():
         return image 
     
     def affine_transform(self, image, scale=1, angle=0, translate=[0, 0], shear=0):
+        image = affine(image, scale=scale, angle=angle, translate=translate, shear=shear)
         image = affine(image, scale=scale, angle=angle, translate=translate, shear=shear)
         return image    
     
@@ -53,20 +58,35 @@ class augmentation_classification_task():
             image = self.affine_transform(image, shear=shear)
         return image
         
-    def run(self, image):
+    def run(self, image, points):
+
+        bboxes = points_to_bboxes(points, image)
         if self.enable_resize:
             image = self.transform_resize(image)
         
         if self.enable_hflip:
-            image = self.horizontal_flip(image)
+            image, bboxes = self.horizontal_flip(image, bboxes.numpy())
             
         if self.enable_vflip:
             image = self.vertical_flip(image)
             
         if self.enable_zoom:
+            print("ZOOM")
             image = self.affine_zoom(image)
             
         if self.enable_shear:
             image = self.affine_shear(image)
         
-        return image   
+        points = tensor(bboxes[:,0:2]).float()
+
+        print(f"type points: {type(points)}" )
+        return image, points
+    
+def points_to_bboxes(points, img):
+    ones_columns = np.ones((points.shape[0], 2))
+    bboxes = np.hstack((points, ones_columns))
+    bboxes = datapoints.BoundingBox(bboxes,
+                                    format=datapoints.BoundingBoxFormat.XYXY,
+                                    spatial_size=transforms.functional.get_spatial_size(img),
+                                    )
+    return bboxes
