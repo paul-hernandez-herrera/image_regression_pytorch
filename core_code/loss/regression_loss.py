1b6bed6a63cc9e5d17b6b73b16bd01e419ebf859
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2023-12-22 15:52:08 -0600

    adding mean for each distance of single file

--- core_code/loss/regression_loss.py
+++ core_code/loss/regression_loss.py
@@ -1,13 +1,15 @@
 import torch
 
-class MSE_points(torch.nn.Module):
+class MDE_points(torch.nn.Module):
     def __init__(self, reduction = 'mean'):
         self.reduction = reduction
         super().__init__()
 
     def forward(self, model_output, target_points):
         # this functions assumes [B, rows, columns]. This function assumes we have B target outputs, with rows indicanting the number of points and columns indicating the dimensión of points
-        loss = torch.sqrt( torch.sum(torch.square(model_output-target_points), dim = 2) )
+        loss = torch.mean(torch.sqrt( torch.sum(torch.square(model_output-target_points), dim = 2) ), dim=1)
+        
         if self.reduction == 'mean':
+            # loss equal to average of batch
             loss = torch.mean(loss)
         return loss
