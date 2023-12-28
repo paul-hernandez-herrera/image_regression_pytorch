1cff813d730d5730d06a25a8a97bada8de5251b4
Paul Hernandez <paul.hernandez@ibt.unam.mx>
2023-12-27 22:36:18 -0600

    changed default reduction to sum

--- core_code/loss/regression_loss.py
+++ core_code/loss/regression_loss.py
@@ -1,7 +1,7 @@
 import torch
 
 class MDE_points(torch.nn.Module):
-    def __init__(self, reduction = 'mean'):
+    def __init__(self, reduction = 'sum'):
         self.reduction = reduction
         super().__init__()
 
@@ -9,7 +9,7 @@ class MDE_points(torch.nn.Module):
         # this functions assumes [B, rows, columns]. This function assumes we have B target outputs, with rows indicanting the number of points and columns indicating the dimensión of points
         loss = torch.mean(torch.sqrt( torch.sum(torch.square(model_output-target_points), dim = 2) ), dim=1)
         
-        if self.reduction == 'mean':
-            # loss equal to average of batch
-            loss = torch.mean(loss)
+        if self.reduction == 'sum':
+            # loss equal to sum of batch
+            loss = torch.sum(loss)
         return loss
